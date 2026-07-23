6549e4f1cf96acb76711c959d1c618c06f6b1dcb
gadiparthiharshini-lgtm <harshini@example.com>
2026-07-23 20:14:50 +0530

    Completed DeepSkilling Modules

--- Deep Skilling/6. ASP.NET Core 8.0 Web API/WebApi_Handson/Models/Department.cs
+++
@@ -1,8 +0,0 @@
-namespace WebApi_Handson.Models;
-
-// Supporting model referenced by Employee (Hands-on 3).
-public class Department
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-}
